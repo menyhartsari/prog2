4616ab84c8562daaadfe162dd6f759f28ff44b3d
menyhartsari <menyhartsari@gmail.com>
2020-06-09 09:46:38 +0200

    2

--- 9.1.13.c
+++ 9.1.13.c
@@ -16,3 +16,55 @@ typedef struct{
     }MAT;
     
 	#define ELEM(M,r,c) (M->elem[(M->cols)*r+c])
+	
+	
+	MAT *mat_create_with_type(unsigned int rows, unsigned int cols){
+        MAT m;
+        MAT *matrix;
+        m.rows = rows;
+        m.cols = cols;
+        m.elem = (float*)malloc(sizeof(float)*rows*cols);
+		matrix = &m;
+       	printf("Matrix inic. rows: %d cols: %d 			%f\n", matrix->rows, matrix->cols, ELEM(matrix, 0,0));
+       	return matrix;
+ 	  }
+ 	  
+ 	MAT *mat_create_by_file(char *filename){
+ 	  	int f;
+        MAT *mat;
+        unsigned int *col, *row;
+        int typ;                       
+        int c,r,i,j;
+        int *u;
+        f=open(filename,O_RDONLY);
+
+      	lseek(f,2*sizeof(char),SEEK_SET);
+        read(f,&row,sizeof(unsigned int));
+        printf("rows: %d ", row);
+        read(f,&col,sizeof(unsigned int));
+        printf("cols: %d\n", col);
+ 		mat = mat_create_with_type(row, col);
+ 		printf("\nrow:%d, col:%d, value:%f\n", mat->rows,mat->cols,ELEM(mat,0,0));
+ /*		 for(i = 0; i < mat->rows; i++){
+            for(j = 0; j < mat->cols; j++){       
+                read(f,&u,sizeof(float));
+                ELEM(mat,i,j) = u;
+                printf("i: %d j: %d val: %f\n",i,j,ELEM(mat,i,j));                	 
+        	} 
+    	}  
+		 printf("\nrow:%d, col:%d, value:%f\n", mat->rows,mat->cols,(mat,0,0));
+       
+       */
+    if(close(f) == EOF){
+        printf("Unable to close file\n");
+        
+    }
+        
+        printf("Matica naèit.\n");
+        return mat;
+    
+    }
+ 	  int main(){
+ 	  	mat_create_with_type(2,2);
+ 	  	return 0;
+	   }
