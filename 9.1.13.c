6ae539b9bf5ec5870f29ceefdf34a8f0ad729a46
menyhartsari <menyhartsari@gmail.com>
2020-06-09 13:43:54 +0200

    10

--- 9.1.13.c
+++ 9.1.13.c
@@ -9,13 +9,17 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-typedef struct{
+	typedef struct{
         unsigned int rows;
         unsigned int cols;
         float *elem;
     }MAT;
     
 	#define ELEM(M,r,c) (M->elem[(M->cols)*r+c])
+	
+	void mat_destroy(MAT *mat){
+        free(mat);
+    }
 
 	MAT *mat_create_with_type(unsigned int rows, unsigned int cols){
         MAT m;
@@ -24,18 +28,21 @@ typedef struct{
         m.cols = cols;
         m.elem = (float*)malloc(sizeof(float)*rows*cols);
 		matrix = &m;
+		if (matrix==0){
+			mat_destroy(matrix);
+			return NULL;
+		}
        	printf("Matrix inic. rows: %d cols: %d 			%f\n", matrix->rows, matrix->cols, ELEM(matrix,0,0));
        	return matrix;
  	  }
- 	
- 	MAT *mat_create_by_file(char *filename){
+
+	MAT *mat_create_by_file(char *filename){
  	  	int f;
         MAT *mat;
         unsigned int col, row;                      
         int i,j;
         float u;
-        if( (f = open(filename, O_BINARY | O_RDONLY)) < 0 )
-		{
+        if( (f = open(filename, O_BINARY | O_RDONLY)) < 0 ){
 			fprintf(stderr, "File access problem.\n");
 			exit(1);
 		}
@@ -53,34 +60,37 @@ typedef struct{
       	    	printf("i: %d j: %d val: %f\n",i,j,ELEM(mat,i,j));                	 
         	} 
     	} 
-    	for ( i = 0; i < mat->rows; i++) {
-            for ( j = 0; j < mat->cols; j++) {
+    	for (i = 0; i < mat->rows; i++) {
+            for (j = 0; j < mat->cols; j++){
                 printf(" %f", ELEM(mat,i,j));
             }
             printf("\n");
-        }
-    	
+        }				
+        if (mat==0){
+        	mat_destroy(mat);
+        	if(close(f) == EOF)
+        		printf("Unable to close file\n");
+        	return 0;
+		}
     	if(close(f) == EOF){
-        printf("Unable to close file\n");
-        
-    }
-        
-        printf("Matrix naèit.\n");
+        	printf("Unable to close file\n");   
+    	}
+        printf("Matrix nacit.\n");
         return mat;
     }
-    
- 	char mat_save(MAT *mat, char *filename){
+
+	char mat_save(MAT *mat, char *filename){
      	int f,i,j;
      	char kod[2]={'M','1'};
-    	if( (f = open(filename, O_BINARY | O_WRONLY)) < 0 ){
+    	if( (f = open(filename, O_BINARY | O_WRONLY | O_CREAT)) < 0 ){
 			fprintf(stderr, "File access problem.\n");
 			exit(1);
 		}
 		write(f,kod,2*sizeof(char));
 		write(f, &mat->rows, sizeof(unsigned int));
 		write(f, &mat->rows, sizeof(unsigned int));
-		for (i=0; i<mat->rows;i++){
-			for (j=0;j<mat->cols;j++)
+		for (i = 0; i < mat->rows; i++){
+			for (j = 0; j < mat->cols; j++)
 				write(f, &ELEM(mat,i,j), sizeof(float));
 		}
     	if(close(f) == EOF){
@@ -89,14 +99,10 @@ typedef struct{
    		}
     }
     
-    void mat_destroy(MAT *mat){
-        free(mat);
-    }
-    
     void mat_unit(MAT *mat){
         int i, j;
-        for ( i = 0; i < mat->rows; i++){
-            for ( j = 0; j < mat->cols; j++){
+        for (i = 0; i < mat->rows; i++){
+            for (j = 0; j < mat->cols; j++){
                 if (i==j)
                     ELEM(mat,i,j)=1; 
 				else
@@ -108,8 +114,8 @@ typedef struct{
     void mat_random(MAT *mat){
         int i, j;
         srand((unsigned int) time(NULL));
-        for ( i = 0; i < mat->rows; i++){
-            for ( j = 0; j < mat->cols; j++){
+        for (i = 0; i < mat->rows; i++){
+            for (j = 0; j < mat->cols; j++){
                 ELEM(mat,i,j)=-1+rand()%3;
             }
         }
@@ -118,8 +124,8 @@ typedef struct{
     void mat_print(MAT *mat){
         int i, j;
         printf("Matrix rows: %d cols: %d \n", mat->rows, mat->cols);
-        for ( i = 0; i < mat->rows; i++) {
-            for ( j = 0; j < mat->cols; j++) {
+        for (i = 0; i < mat->rows; i++) {
+            for (j = 0; j < mat->cols; j++) {
                 printf(" %f", ELEM(mat,i,j));
             }
             printf("\n");
@@ -163,9 +169,8 @@ typedef struct{
         }
         return rank;
     }
-	
- 	int main(){
- 	  	mat_create_with_type(2,2);
+
+	int main(){
  	 	MAT *A = mat_create_by_file("matrix.txt");
  	 	mat_rank(A);
  	 	printf("Hodnost matice je : %d", mat_rank(A)); 
