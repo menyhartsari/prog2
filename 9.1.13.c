a2f29c453bfca0f746d13669ce8739d05c8d2a55
menyhartsari <menyhartsari@gmail.com>
2020-06-09 11:14:06 +0200

    save, destroy, unit...(5)

--- 9.1.13.c
+++ 9.1.13.c
@@ -24,37 +24,39 @@ typedef struct{
         m.cols = cols;
         m.elem = (float*)malloc(sizeof(float)*rows*cols);
 		matrix = &m;
-       	printf("Matrix inic. rows: %d cols: %d 			%f\n", matrix->rows, matrix->cols, ELEM(matrix,1,1));
+       	printf("Matrix inic. rows: %d cols: %d 			%f\n", matrix->rows, matrix->cols, ELEM(matrix,0,0));
        	return matrix;
  	  }
  	  
  	  MAT *mat_create_by_file(char *filename){
  	  	int f;
         MAT *mat;
-        unsigned int c,r,col, row;
-        int typ;                       
+        unsigned int c,r,col, row;                      
         int i,j;
         float u;
-        f=open(filename,O_RDONLY);
-        sizeof(unsigned int) == sizeof(void );
+        if( (f = open(filename, O_BINARY | O_RDONLY)) < 0 )
+		{
+			fprintf(stderr, "File access problem.\n");
+			exit(1);
+		}
       	lseek(f,2*sizeof(char),SEEK_SET);
         read(f,&row,sizeof(unsigned int));
-        printf("rows %d ",row);
+        printf("rows: %d ", row);
         read(f,&col,sizeof(unsigned int));
-        printf("cols %d\n", col);
+        printf("cols: %d\n", col);
  		mat = mat_create_with_type(row, col);
- 		printf("\nrow: %d, col: %d, value: %f\n", mat->rows,mat->cols,ELEM(mat,0,0));
- 		r=3;
- 		c=3;
- 	 	for(i = 0; i= r; i++){
-            for(j = 0; j =c; j++){      
-                read(f,mat->elem,sizeof((float*)malloc(sizeof(float)*r*c)));
-            	ELEM(mat,i,j) = 11;
-      	    	printf("i %d j %d val %f\n",i,j,ELEM(mat,i,j));                	 
+ 		printf("\nrow:%d, col:%d, value:%f\n", mat->rows,mat->cols,ELEM(mat,0,0));
+ 		r=mat->rows;
+ 		c=mat->cols;
+ 	 	for(i = 0; i < r; i++){
+            for(j = 0; j < c; j++){      
+                read(f,&u,sizeof(float));
+            	ELEM(mat,i,j) = u;
+      	    	printf("i: %d j: %d val: %f\n",i,j,ELEM(mat,i,j));                	 
         	} 
     	} 
-    	for ( i = 0; i =mat->rows; i++) {
-            for ( j = 0; j = mat->cols; j++) {
+    	for ( i = 0; i < mat->rows; i++) {
+            for ( j = 0; j < mat->cols; j++) {
                 printf(" %f", ELEM(mat,i,j));
             }
             printf("\n");
@@ -69,13 +71,54 @@ typedef struct{
         return mat;
     
     }
+ 	char mat_save(MAT *mat, char *filename){
+     	int f;
+     	char kod[2]={'M','1'};
+    	if( (f = open(filename, O_BINARY | O_WRONLY)) < 0 ){
+			fprintf(stderr, "File access problem.\n");
+			exit(1);
+		}
+		write(f,kod,2*sizeof(char));
+		write(f, &mat, (MAT*)malloc(sizeof(MAT)));
+    	if(close(f) == EOF){
+        printf("Unable to close file\n");
+        return -1;
+   		}
+    }
+    
+    void mat_destroy(MAT *mat){
+        free(mat);
+    }
+    
+    void mat_unit(MAT *mat){
+        int i, j;
+        for ( i = 0; i < mat->rows; i++) {
+            for ( j = 0; j < mat->cols; j++) {
+                if (i==j) {
+                    ELEM(mat,i,j)=1; 
+                } else {
+                    ELEM(mat,i,j)=0;
+                }
+            }
+        }
+    }
+    
+    void mat_random(MAT *mat){
+        int i, j;
+        srand((unsigned int) time(NULL));
+        for ( i = 0; i < mat->rows; i++) {
+            for ( j = 0; j < mat->cols; j++) {
+                ELEM(mat,i,j)=-1+rand()%3;
+            }
+        }
+    }
     
     void mat_print(MAT *mat){
         int i, j;
         printf("Matrix rows: %d cols: %d \n", mat->rows, mat->cols);
-        for ( i = 0; i = mat->rows; i++) {
-            for ( j = 0; j = mat->cols; j++) {
-                printf("%d ", ELEM(mat,i,j));
+        for ( i = 0; i < mat->rows; i++) {
+            for ( j = 0; j < mat->cols; j++) {
+                printf(" %f", ELEM(mat,i,j));
             }
             printf("\n");
         }
