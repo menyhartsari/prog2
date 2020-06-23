0e9d6d659278cd2613dae98921f4e304b9442cc5
peter.sarkoci@occolator <peter.sarkoci@gmail.com>
2020-06-23 11:11:37 +0100

    dve funkcie na elemetarne riadkove operacie a vypocet hodnosti s ich pomocou a potom jeden novy test vo funkcii main

--- 9.1.13.c
+++ 9.1.13.c
@@ -148,6 +148,27 @@ void mat_print(MAT *mat){
     }
 }
 
+void mat_swap_rows(MAT *m, unsigned int i, unsigned int j)
+{
+    unsigned int k;
+    float aux;
+
+    for(k=0; k<m->cols; k++)
+    {
+        aux = ELEM(m,i,k);
+        ELEM(m,i,k) = ELEM(m,j,k);
+        ELEM(m,j,k) = aux;
+    }
+}
+
+void mat_admul_rows(MAT *m, unsigned int i, float mul, unsigned int j)
+{
+    unsigned int k;
+
+    for(k=0; k<m->cols; k++)
+        ELEM(m,j,k) += mul*ELEM(m,i,k);
+}
+
 unsigned int mat_rank(MAT *mat){
     int rank, row, i, j, bin, justnull, other_row;
     float x;
@@ -162,27 +183,33 @@ unsigned int mat_rank(MAT *mat){
 			for (other_row = 0; other_row < mat->rows; other_row++){ 
            		if (other_row != row){
 					x = (ELEM(mat,other_row,row) / ELEM(mat,row,row));
+                    /*
             		for (i = 0; i < mat->rows; i++)
 						ELEM(mat,other_row,i) -= x * ELEM(mat,row,i); 
+                    */
+                    mat_admul_rows(mat,row,-x,other_row);
           		} 
        		}
     	}
 		else{
         	justnull=1;
-    		for (i = row + 1; i < mat->rows;  i++){         
+    		for (i = row + 1; i < mat->rows; i++){
 				if (ELEM(mat, i, row) != 0){
+                    /*
              		for (j = 0; j < rank; j++){
                     	bin = ELEM(mat,row,j);
 						ELEM(mat,row,j) = ELEM(mat,i,j); 
                     	ELEM(mat,i,j) = bin; 
                 	}
+                    */
+                    mat_swap_rows(mat,row,i);
                 	justnull=0;
                 	break; 
             	}
         	} 
     		if (justnull==1){  
             	rank--; 
-            	for (i = 0; i < mat->rows; i ++) 
+            	for (i = 0; i < mat->rows; i++) 
                 	ELEM(mat,i,row) = ELEM(mat,i,rank);
         	} 
         	row--;
@@ -195,7 +222,11 @@ unsigned int mat_rank(MAT *mat){
 int main(){
  	MAT *A = mat_create_by_file("matrix.txt");
     MAT *B = mat_create_with_type(3,3);
+    MAT *C = mat_create_with_type(3,4);
     unsigned int rank;
+
+    rank = mat_rank(A);
+	printf("Hodnost matice A je : %d\n", rank);
 
     ELEM(B,0,0) = 1;
     ELEM(B,0,1) = 1;
@@ -207,10 +238,23 @@ int main(){
     ELEM(B,2,1) = 1;
     ELEM(B,2,2) = 1;
 
-    rank = mat_rank(A);
-	printf("Hodnost matice A je : %d\n", rank);
-
     rank = mat_rank(B);
 	printf("Hodnost matice B je : %d\n", rank);
+
+    ELEM(C,0,0) = 1;
+    ELEM(C,0,1) = 1;
+    ELEM(C,0,2) = 1;
+    ELEM(C,0,3) = 1;
+    ELEM(C,1,0) = 1;
+    ELEM(C,1,1) = 1;
+    ELEM(C,1,2) = 1;
+    ELEM(C,1,3) = 0;
+    ELEM(C,2,0) = 1;
+    ELEM(C,2,1) = 1;
+    ELEM(C,2,2) = 1;
+    ELEM(C,2,3) = 1;
+
+    rank = mat_rank(C);
+	printf("Hodnost matice C je : %d\n", rank);
 	return 0;
 }
