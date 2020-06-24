b4589548f121e2181d9134972d41ead46194dd19
menyhartsari <menyhartsari@gmail.com>
2020-06-24 16:27:11 +0200

    oprava

--- 9.1.13.c
+++ 9.1.13.c
@@ -170,52 +170,44 @@ void mat_admul_rows(MAT *m, unsigned int i, float mul, unsigned int j)
 }
 
 unsigned int mat_rank(MAT *mat){
-    int rank, row, i, j, bin, justnull, other_row;
+    int rank, i, j, k, justnull, row;
     float x;
-
-    if(mat->cols > mat->rows){
-        rank = mat->rows;
-    } else {
-        rank = mat->cols;
+    
+    row = 0;
+    for (i = 0; i < mat->cols; i++){
+        for (j = row; j < mat->rows; j++){
+            if (ELEM(mat,j,i) != 0){
+                mat_swap_rows(mat,j,row);
+                row++;
+            }
+        }
     }
-	for (row = 0; row < rank; row++){    
-    	if (ELEM(mat,row,row) != 0){           
-			for (other_row = 0; other_row < mat->rows; other_row++){ 
-           		if (other_row != row){
-					x = (ELEM(mat,other_row,row) / ELEM(mat,row,row));
-                    /*
-            		for (i = 0; i < mat->rows; i++)
-						ELEM(mat,other_row,i) -= x * ELEM(mat,row,i); 
-                    */
-                    mat_admul_rows(mat,row,-x,other_row);
-          		} 
-       		}
-    	}
-		else{
-        	justnull=1;
-    		for (i = row + 1; i < mat->rows; i++){
-				if (ELEM(mat, i, row) != 0){
-                    /*
-             		for (j = 0; j < rank; j++){
-                    	bin = ELEM(mat,row,j);
-						ELEM(mat,row,j) = ELEM(mat,i,j); 
-                    	ELEM(mat,i,j) = bin; 
-                	}
-                    */
-                    mat_swap_rows(mat,row,i);
-                	justnull=0;
-                	break; 
-            	}
-        	} 
-    		if (justnull==1){  
-            	rank--; 
-            	for (i = 0; i < mat->rows; i++) 
-                	ELEM(mat,i,row) = ELEM(mat,i,rank);
-        	} 
-        	row--;
-    	}
+    for (i = 0; i < mat->rows; i++){
+        for (j = 0; j < mat->cols; j++){
+            if (ELEM(mat,i,j) != 0){
+                for (k = 0; k < mat->cols; k++)
+                    ELEM(mat,i,k) = ELEM(mat,i,k)/ELEM(mat,i,j);
+            	for (k = 0; k < mat->rows; k++){
+                    if (k != i && ELEM(mat,k,j) != 0){
+                        x = ELEM(mat,k,j);
+						mat_admul_rows(mat,i,-x,k);
+                    }
+                }
+            	break;
+            }
+        } 
     }
-
+    rank = 0;
+    for (i = 0; i < mat->rows; i++){
+        justnull = 0;
+        for (j = 0; j < mat->cols; j++){
+            if (ELEM(mat,i,j) != 0)
+                justnull = 1;
+	    	else
+            	ELEM(mat,i,j) = 0;            
+        }
+        rank+=justnull;
+    }    
     return rank;
 }
 
@@ -223,9 +215,10 @@ int main(){
  	MAT *A = mat_create_by_file("matrix.txt");
     MAT *B = mat_create_with_type(3,3);
     MAT *C = mat_create_with_type(3,4);
+    MAT *D = mat_create_with_type(5,3);
     unsigned int rank;
-
-    rank = mat_rank(A);
+	
+	rank = mat_rank(A);
 	printf("Hodnost matice A je : %d\n", rank);
 
     ELEM(B,0,0) = 1;
@@ -256,5 +249,24 @@ int main(){
 
     rank = mat_rank(C);
 	printf("Hodnost matice C je : %d\n", rank);
+	
+	ELEM(D,0,0) = 1;
+    ELEM(D,0,1) = 1;
+    ELEM(D,0,2) = 1;
+    ELEM(D,1,0) = 1;
+    ELEM(D,1,1) = 0;
+    ELEM(D,1,2) = 1;
+    ELEM(D,2,0) = 1;
+    ELEM(D,2,1) = 4;
+    ELEM(D,2,2) = 1;
+    ELEM(D,3,0) = 3;
+    ELEM(D,3,1) = 1;
+    ELEM(D,3,2) = 0;
+    ELEM(D,4,0) = 1;
+    ELEM(D,4,1) = 1;
+    ELEM(D,4,2) = 1;
+
+	rank = mat_rank(D);
+	printf("Hodnost matice D je : %d\n", rank);
 	return 0;
 }
